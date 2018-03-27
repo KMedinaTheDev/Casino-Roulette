9a3f9353a0fc09689cd733d6ee3fe4b70e5fa5d5
Karen Medina <KMedina.WebDev+github@gmail.com>
2018-03-27 11:46:28 -0400

    db updated for roulette

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://derp:derp1@ds215019.mlab.com:15019/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://admin:admin@ds125489.mlab.com:25489/roulette-app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
